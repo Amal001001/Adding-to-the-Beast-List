0e4dafd9c26c8e2e4036153b856476686edd26e6
Amal001001 <amalalmutairi001@gmail.com>
2021-12-08 04:55:45 +0000

    added Deleting by taping feature

--- Adding to the Beast List/ViewController.swift
+++ Adding to the Beast List/ViewController.swift
@@ -29,15 +29,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
 }
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return tasks.count
     }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
                 
@@ -46,5 +49,11 @@ extension ViewController: UITableViewDataSource {
             return cell
     }
     
-    
+    //UITableViewDelegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        tasks.remove(at: indexPath.row)
+        tableView.reloadData()
+    }
 }
+
