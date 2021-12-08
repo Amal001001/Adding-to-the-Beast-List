0ab46ba42468fbb4cc81d05a4cf5b105223eb56a
Amal001001 <amalalmutairi001@gmail.com>
2021-12-08 04:44:41 +0000

    Initial Commit

---
+++ Adding to the Beast List/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  Adding to the Beast List
+//
+//  Created by admin on 08/12/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var tasks = ["Exercise for 30 minutes", "Wireframe for some project", "Do laundry"]
+    var newtask:String!
+    
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        newtask = taskTextField.text!
+        
+           if newtask != nil && newtask != ""
+           {
+             tasks.append(newtask)
+           }
+        taskTextField.text = ""
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+    }
+
+
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+                
+            cell.textLabel?.text = tasks[indexPath.row]
+                
+            return cell
+    }
+    
+    
+}
---
+++ Adding to the Beast ListUITests/Adding_to_the_Beast_ListUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Adding_to_the_Beast_ListUITestsLaunchTests.swift
+//  Adding to the Beast ListUITests
+//
+//  Created by admin on 08/12/2021.
+//
+
+import XCTest
+
+class Adding_to_the_Beast_ListUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
